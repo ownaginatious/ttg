76515035b375d358103713c88d32945a710d2922
Dillon Dixon <dillondixon@gmail.com>
2016-11-22 15:45:07 -0800

    fixed uottawa parser

--- src/main/java/com/timetablegenerator/scraper/school/uottawa/UniversityOfOttawaScraper.java
+++ src/main/java/com/timetablegenerator/scraper/school/uottawa/UniversityOfOttawaScraper.java
@@ -15,8 +15,6 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -48,18 +46,14 @@ public class UniversityOfOttawaScraper extends Scraper {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static final Set<String> NON_COLLAPSING_SECTIONS = new HashSet<>(
-            Arrays.asList("DGD", "LAB", "TUT")
-    );
-
-    private static final Pattern COURSE_CODE_PATTERN = Pattern.compile("^(?<code>[A-Z]+[0-9]+[A-Z]?) (?<section>[A-Z0-9]+ )?.*");
-    private static final Pattern SECTION_PATTERN = Pattern.compile("^(?<section>.+) (?<number>[0-9]+)$");
+    private static final Pattern COURSE_CODE_PATTERN =
+            Pattern.compile("^(?<code>[A-Z]+\\s?[0-9]+\\s?[A-Z]*) (?<section>(?<variant>[A-Z]*)[0-9]*)( " +
+                    "\\((?<smonth>[A-Z][a-z]+) (?<sday>[0-9]+) - (?<emonth>[A-Z][a-z]+) (?<eday>[0-9]+)\\))?");
     private static final Pattern DAY_TIME_PATTERN =
-            Pattern.compile("^(?<day>[A-Z][a-z]+) (?<shour>[0-9]{2}):(?<sminute>[0-9]{2}) - (?<ehour>[0-9]{2}):(?<eminute>[0-9]{2})$");
+            Pattern.compile("^(?<day>[A-Z][a-z]+) " +
+                    "(?<shour>[0-9]{1,2}):(?<sminute>[0-9]{2}) - (?<ehour>[0-9]{1,2}):(?<eminute>[0-9]{2})$");
 
     private static final String UNAVAILABLE = "Not available at this time.";
-    private static final String TBA_TIME = "00:00 - 00:00";
-    private static final String ONLINE_INDICATOR = "Course entirely via Internet";
 
     private static final String TIMETABLE_ROOT = "https://web30.uottawa.ca/v3/SITS/timetable/Search.aspx";
     private static final String RESULTS_ROOT = "https://web30.uottawa.ca/v3/SITS/timetable/SearchResults.aspx";
@@ -70,7 +64,8 @@ public class UniversityOfOttawaScraper extends Scraper {
         String eventValidation = responseElement.getElementById("__EVENTVALIDATION").attr("value");
         String viewState = responseElement.getElementById("__VIEWSTATE").attr("value");
 
-        return req.setFormParameter("__EVENTVALIDATION", eventValidation).setFormParameter("__VIEWSTATE", viewState);
+        return req.setFormParameter("__EVENTVALIDATION", eventValidation)
+                .setFormParameter("__VIEWSTATE", viewState);
     }
 
     @Override
@@ -94,16 +89,18 @@ public class UniversityOfOttawaScraper extends Scraper {
             LOGGER.info("Retrieving courses under department " + d + " [" + ++i + "/" + departments.size() + "]...");
 
             rr = addState(searchPageElement, rr.nextPost(TIMETABLE_ROOT)
-                    .setFormParameter("ctl00$MainContentPlaceHolder$Basic_SessionDropDown", term.getKey())
-                    .setFormParameter("ctl00$MainContentPlaceHolder$Cancel_SessionDropDown", term.getKey())
+                    .setFormParameter("ctl00$MainContentPlaceHolder$Basic_TermDropDown", term.getKey())
                     .setFormParameter("ctl00$MainContentPlaceHolder$Basic_SubjectDropDown", d.getCode())
-                    .setFormParameter("ctl00$MainContentPlaceHolder$Basic_Button", "Search")).run();
+                    .setFormParameter("ctl00$MainContentPlaceHolder$Advance_CampusChoice", "UOTTA")
+                    .setFormParameter("ctl00$MainContentPlaceHolder$Basic_Button", "Search")
+            ).run();
 
             boolean hasMore;
 
             Element responseElement;
 
             Map<String, String> courseNameMap = new HashMap<>();
+            Map<String, Course> discoveredCourses = new HashMap<>();
 
             while (true) {
 
@@ -114,8 +111,16 @@ public class UniversityOfOttawaScraper extends Scraper {
                 responseElement.select("table.result-table > tbody > tr:not(.results-header)")
                         .forEach(x -> courseNameMap.put(x.select(".CourseCode").text(), x.select(".CourseTitle").text()));
 
-                if (!hasMore)
+                // Parse the course data referenced by this page.
+                Element schedulingElements = Jsoup.parse(rr.nextGet(PRINT_ROOT).run().getResponseString());
+
+                for (Element schedulingElement : schedulingElements.select("#main-content div.schedule")) {
+                    parseCourseData(term, d, courseNameMap, discoveredCourses, schedulingElement);
+                }
+
+                if (!hasMore) {
                     break;
+                }
 
                 LOGGER.debug(" >> Getting more data for department " + d.getCode() + "...");
 
@@ -124,24 +129,16 @@ public class UniversityOfOttawaScraper extends Scraper {
             }
 
             LOGGER.debug(" >> No more data for department " + d.getCode() + "!");
-
-            Collection<Course> discoveredCourses =
-                    Jsoup.parse(rr.nextGet(PRINT_ROOT).run().getResponseString()).select("#main-content div.schedule")
-                            .stream().map(c -> parseCourseData(term, d, courseNameMap, c))
-                            .flatMap(Collection::stream).collect(Collectors.toList());
-
             LOGGER.info(" >> " + discoveredCourses.size() + " courses discovered.");
 
-            discoveredCourses.forEach(tt::addCourse);
+            discoveredCourses.values().forEach(tt::addCourse);
         }
 
         return tt;
     }
 
-    private Collection<Course> parseCourseData(Term term, Department department,
-                                               Map<String, String> courseNameMap, Element courseElement) {
-
-        Collection<Course> courses = new ArrayList<>();
+    private void parseCourseData(Term term, Department department, Map<String, String> courseNameMap,
+                                 Map<String, Course> courseMap, Element courseElement) {
 
         for (Element tableElement : courseElement.select("table.display > tbody")) {
 
@@ -158,18 +155,31 @@ public class UniversityOfOttawaScraper extends Scraper {
                 throw new IllegalArgumentException("Cannot parse course code from \"" + courseCodeString + "\"");
 
             String courseCode = m.group("code");
-            String courseSection = m.group("section") == null ? "" : m.group("section").trim();
+            String courseVariant = m.group("variant");
+            String sectionCode = m.group("section");
 
             String courseName = courseNameMap.get(courseCode);
 
-            if (courseName == null)
+            if (courseName == null) {
                 throw new IllegalStateException("No matching course name for course code \"" + courseCode + "\"");
+            }
 
             // FIXME: Find out how many credits each course is actually worth.
-            Course c = new Course(getSchool(), term.getTermId(), department,
-                    courseCode + " " + courseSection, courseName, 3.0);
 
-            courses.add(c);
+            String courseLocalIdentifier = courseCode;
+            if (courseVariant != null && courseVariant.trim().length() > 0){
+                courseLocalIdentifier += " " + courseVariant;
+            }
+
+            Course c;
+
+            if (courseMap.containsKey(courseLocalIdentifier)) {
+                c = courseMap.get(courseLocalIdentifier);
+            } else {
+                c = new Course(getSchool(), term.getTermId(), department,
+                        courseLocalIdentifier, courseName, 3.0);
+                courseMap.put(courseLocalIdentifier, c);
+            }
 
             rowIterator.previous();
 
@@ -177,38 +187,24 @@ public class UniversityOfOttawaScraper extends Scraper {
 
                 Element rowElement = rowIterator.next();
 
-                String activityString = rowElement.select(".Activity").text();
+                String sectionType = rowElement.select(".Activity").text();
                 String dayTimeString = rowElement.select(".Day").text();
                 String placeString = rowElement.select(".Place").text();
                 String professorString = rowElement.select(".Professor").text();
 
-                String sectionCode;
-                String sectionNumber;
-
-                boolean courseIsOnline = activityString.startsWith(ONLINE_INDICATOR);
-
-                m = SECTION_PATTERN.matcher(activityString);
-
-                if (!m.find())
-                    throw new IllegalArgumentException("Cannot parse section type from \"" + activityString + "\"");
-
-                sectionCode = getSchool().getSectionTypeCodeByName(m.group("section").trim());
-                sectionNumber = m.group("number");
-
                 Section s;
 
-                if (!NON_COLLAPSING_SECTIONS.contains(sectionCode)
-                        && c.getSectionType(sectionCode) != null
-                        && c.getSectionType(sectionCode).getSection("1") != null)
-                    s = c.getSectionType(sectionCode).getSection("1");
+                if (c.getSectionType(sectionType) != null
+                        && c.getSectionType(sectionType).getSection(sectionCode) != null)
+                    s = c.getSectionType(sectionType).getSection(sectionCode);
                 else {
-                    s = Section.fromName(sectionNumber);
-                    c.addSection(sectionCode, s);
+                    s = Section.fromName(sectionCode);
+                    c.addSection(sectionType, s);
                 }
 
                 RepeatingPeriod rp = new RepeatingPeriod(term.getTermId());
 
-                if (!dayTimeString.equals(TBA_TIME) && !courseIsOnline) {
+                if (!dayTimeString.equals(UNAVAILABLE)) {
 
                     m = DAY_TIME_PATTERN.matcher(dayTimeString);
 
@@ -249,22 +245,17 @@ public class UniversityOfOttawaScraper extends Scraper {
                             LocalTime.of(Integer.parseInt(m.group("ehour")), Integer.parseInt(m.group("eminute"))));
                 }
 
-                if (!professorString.equals(UNAVAILABLE))
+                if (!professorString.equals(UNAVAILABLE)) {
                     rp.addSupervisors(professorString);
+                }
 
-                if (!courseIsOnline)
+                if (!placeString.equals(UNAVAILABLE)) {
                     rp.setRoom(placeString);
-
-                s.setOnline(courseIsOnline);
+                }
 
                 s.addPeriod(rp);
             }
         }
-
-        // Make all the courses anti-requisites of one another.
-        courses.forEach(x -> courses.stream().filter(y -> x != y).forEach(x::addAntirequisite));
-
-        return courses;
     }
 
     @Override
@@ -277,7 +268,7 @@ public class UniversityOfOttawaScraper extends Scraper {
 
         Set<Term> terms = new HashSet<>();
 
-        termsElement.getElementById("ctl00_MainContentPlaceHolder_Basic_SessionDropDown")
+        termsElement.getElementById("ctl00_MainContentPlaceHolder_Basic_TermDropDown")
                 .select("option").stream().filter(e -> e.attr("value") != null && !e.attr("value").isEmpty())
                 .forEach(e -> terms.add(Term.findProbableTerm(e.text(), e.attr("value"))));
 
